241285310746b9dc377861905aed515868e311d5
Rey <reycrisbf@gmail.com>
2024-02-18 22:10:08 -0400

    Se realizo la relacion entre la entidad Productos y Ubicaciones, donde un producto puede estar disponible en multiples ubicaciones y una ubicacion puede contener multiples productos.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
@@ -17,5 +18,9 @@ class Product extends Model
 
     public function sale () : BelongsTo {
         return $this->belongsTo(Sale::class);
+    }
+
+    public function location () : BelongsToMany {
+        return $this->belongsToMany(Location::class);
     }
 }
