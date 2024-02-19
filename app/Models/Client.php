9339f1ed0a65b712c022dfda748ba233bbe59632
Rey <reycrisbf@gmail.com>
2024-02-18 21:48:32 -0400

    Se hizo la relacion entre la entidad ventas y clientes, donde un cliente puede realizar varias ventas, pero esta venta esta asociada a un unico cliente

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Client extends Model
 {
@@ -13,5 +14,9 @@ class Client extends Model
 
     public function group () : BelongsTo{
         return $this->belongsTo(Group::class);
+    }
+
+    public function sales () : HasMany{
+        return $this->hasMany(Sale::class);
     }
 }
--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Sale extends Model
 {
     use HasFactory;
     protected $table = "sales";
+    
+    public function client () : BelongsTo {
+        return $this->belongsTo(Client::class);
+    }
 }
