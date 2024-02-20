9efad08401b06bb640f587522d77f7c80dc3dc58
DilandCastellon <gustavocastellon63@gmail.com>
2024-02-20 17:03:29 -0400

    Added default value for the 'new_field' column in the 'categories' migration

--- database/migrations/2024_02_06_183325_create_categories_table.php
+++ database/migrations/2024_02_06_183325_create_categories_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('detail', 250);
             $table->string('status', 15);
             $table->timestamps();
-            $table->string('new_field', 50);
+            $table->string('new_field', 50)->default('sin_asignar'); // Valor predeterminado 'sin_asignar'
         });
     }
 
